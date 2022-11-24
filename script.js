e86562a9adcee95b5da0ae8bdf47d152f352a5f0
zyedibrahim <zyedibrahim77@gmail.com>
2022-11-24 15:13:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let Resume = {
+
+Name: "Annomyus",
+age: "25",
+experience: "5Years",
+gender: "male",
+location: "chennai",
+language_Know: "Englis , Tamil",
+skill:"Full Stack Developer",
+}
+
+//  using for loops
+for (i= 1 ;i<=jsonEx.length;i++){
+     console.log(jsonEx[i]);
+}
